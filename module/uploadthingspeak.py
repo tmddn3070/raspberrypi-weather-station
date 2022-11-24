3e52b36329631d86f04a6555aa80d6e04ea78efd
SMOO <69005921+tmddn3070@users.noreply.github.com>
2022-11-24 09:32:42 +0900

    Update uploadthingspeak.py

--- module/uploadthingspeak.py
+++ module/uploadthingspeak.py
@@ -12,28 +12,18 @@ def thingspeak(apikey,temperature,humidity,rainfall,lux,finedust1,finedust25,fin
         url = "https://api.thingspeak.com/update?api_key="+apikey
         url = url + "&field1="+temperature
         url = url + "&field2="+humidity
-        url = url + "&field3="+rainfall
-        url = url + "&field4="+lux
         url = url + "&field5="+finedust1
         url = url + "&field6="+finedust25
         url = url + "&field7="+finedust10
         url = url + "&field8="+cputemperature
         response = requests.get(url, timeout=30)
-    except requests.excetions.ConnectionError as errc:
-        inetcheck = internetcheck(5)
-        if inetcheck == False:
-            insertlogh("failed to upload thinspeak(reason: conn err)")
-        elif inetcheck == True:
-            pass
-        return False, errcon
-    except requests.excetions.Timeout as errt:
-        check == internetcheck(15)
-        if check == False:
-            insertlogh("failed to upload thinspeak(reason: conn err)")
-        elif check == True:
-            pass
-        return False, errtime
-    elif 
-        return True, response.text
+        result = True
+        error = None
+        return result, error
+    except as err:
+        result = False
+        error = err
+        return result, error
+        
 
 
